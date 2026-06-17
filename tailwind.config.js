7223fc4f49c1d31d0fc4cbcb3411f9ff103c6edc
Gaatha Patel <140440288+gaathapatel07@users.noreply.github.com>
2026-06-18 00:03:45 +0530

    Delete tailwind.config.js

--- tailwind.config.js
+++
@@ -1,6 +0,0 @@
-export default {
-  darkMode:'class',
-  content:['./index.html','./src/**/*.{js,ts,jsx,tsx}'],
-  theme:{extend:{fontFamily:{sans:['Inter','system-ui','sans-serif'],mono:['JetBrains Mono','monospace']}}},
-  plugins:[],
-}
